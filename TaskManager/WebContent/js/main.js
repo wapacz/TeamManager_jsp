d1773e4186b393cd366a4f322d3c706f39777c18
Michal Lapacz <michal.lapacz@gmail.com>
2012-06-05 14:09:58 +0200

    initial version

---
+++ TaskManager/WebContent/js/main.js
@@ -0,0 +1,4 @@
+function initialize() {
+    $("#output").html("hello from jQuery");
+
+}
---
+++ TaskManager/src/org/catcher/TaskManager/DAO/Proxy.java
@@ -0,0 +1,57 @@
+package org.catcher.TaskManager.DAO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Proxy
+ */
+@WebServlet(description = "Used for accessig DB via HTTP requests", urlPatterns = { "/Proxy" })
+public class Proxy extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Proxy() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// request.getParameter("id");
+		PrintWriter out = response.getWriter();
+		out.println("Hello World from servlet!!!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
